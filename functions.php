c7e5aa7e8a76ece5e8c9d9ba1c8adee96943d50a
dennytrumpet <dmmcghee3@gmail.com>
2017-02-18 13:54:10 -0600

    registering menu area
    making menus

--- functions.php
+++ functions.php
@@ -2,7 +2,13 @@
 
 add_action('wp_enqueue_scripts', 'tsc_wp_enqueue_scripts');
 
- function tsc_wp_enqueue_scripts() {
-     wp_enqueue_style('twentyseventeen-style', get_template_directory_uri() . '/style.css');
-     wp_enqueue_style('twentyserpenteen-style', get_stylesheet_directory_uri() . '/style.css');
- }
+function tsc_wp_enqueue_scripts() {
+    wp_enqueue_style('twentyseventeen-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('twentyserpenteen-style', get_stylesheet_directory_uri() . '/style.css');
+}
+
+add_action('after_setup_theme', 'tsc_after_setup_theme');
+
+function tsc_after_setup_theme() {
+    register_nav_menu('evergreen-content', __('Evergreen Menu', 'twentyserpenteen'));
+}
--- templates/welcome-to-our-blog.php
+++ templates/welcome-to-our-blog.php
@@ -6,7 +6,13 @@ get_header(); ?>
         <div id="primary" class="content-area">
             <main id="main" class="site-main" role="main">
                 <h2><?php the_title();?></h2>
-
+                <p>Check out these fuckin posts:</p>
+                <hr />
+                <?php wp_nav_menu( array(
+                    'theme_location' => 'evergreen-content',
+                    'menu_class' => 'evergreen-content',
+                )); ?>
+                <hr />
             </main><!-- #main -->
         </div><!-- #primary -->
     </div><!-- .wrap -->
