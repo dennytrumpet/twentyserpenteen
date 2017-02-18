89d02145c4d60c7dc6487d8f34b1bb0ad078e386
dennytrumpet <dmmcghee3@gmail.com>
2017-02-18 13:05:51 -0600

    Adding a custom template
    welcome page

---
+++ welcome-to-our-blog.php
@@ -0,0 +1,14 @@
+<?php // Template Name: Welcome to Our Blog
+
+get_header(); ?>
+
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+                <h2><?php the_title();?></h2>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
+
+<?php get_footer();
